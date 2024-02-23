f807624b4f3d656991e4cad268d1a23ec967dd59
zippyh <zippyh@icloud.com>
2024-02-23 13:12:07 -0500

    Added error message for incorrect input

--- AnimalASCIIArt.java
+++ AnimalASCIIArt.java
@@ -5,7 +5,14 @@ public class AnimalASCIIArt {
         Scanner scan = new Scanner(System.in);
         System.out.println("What animal would you like to see(cat or dog)?");
         String choice;
-        choice = scan.next();
+        while(true) {
+            choice = scan.next();
+            if(choice.toLowerCase().equals("cat") || choice.toLowerCase().equals("dog")) {
+                break;
+            } else {
+                System.out.println("I don't know that one, try again.");
+            }
+        }
 
         if(choice.equals("cat")) {
             System.out.println("CAT ASCII ART HERE");
