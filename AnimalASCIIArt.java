0b8c889a57f2b7612e6eda36670a8fb4f5015410
github user name <github email>
2024-02-29 17:23:37 -0500

    Added moose art

--- AnimalASCIIArt.java
+++ AnimalASCIIArt.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class AnimalASCIIArt {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("What animal would you like to see(cat, dog, or fish)?");
+        System.out.println("What animal would you like to see(cat, dog,fish or moose)?");
         String choice;
         while(true) {
             choice = scan.next();
             if(choice.toLowerCase().equals("cat") || choice.toLowerCase().equals("dog") ||
-            choice.toLowerCase().equals("fish")) {
+            choice.toLowerCase().equals("fish")||choice.equals("moose")) {
                 break;
             } else {
                 System.out.println("I don't know that one, try again.");
@@ -19,7 +19,14 @@ public class AnimalASCIIArt {
             System.out.println(" _._     _,-\'\"\"`-._\n(,-.`._,\'(       |\\`-/|\n    `-.-\' \\ )-`( , o o)\n          `-    \\`_`\"\'-");
         } else if(choice.equals("dog")) {
             System.out.println("           __\n      (___()\'`;\n      /,    /`\n      \\\\\"--\\\\");
-        } else {
+        }
+        else if(choice.equals("moose"))
+        {
+            System.out.println(
+" ___            ___\n/   \\          /   \\\n\\_   \\        /  __/\n _\\   \\      /  /__\n \\___  \\____/   __/\n     \\_       _/\n       | @ @  \\_\n       |\n     _/     /\\\n    /o)  (o/\\ \\_\n    \\_____/ /\n      \\____/"
+);
+        }
+         else {
             System.out.println("|\\   \\\\\\\\__     o\n| \\_/    o \\    o \n> _   (( <_  oo  \n| / \\__+___/      \n|/     |/");
         }
         scan.close();
